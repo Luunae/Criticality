b5bb91dc262e773526d335fdc8146232de9fd549
Luna <github@luna.allan.scot>
2019-09-29 09:34:42 +0100

    Add reactor sim test

---
+++ test_reactor.py
@@ -0,0 +1,15 @@
+from reactor import Reactor
+
+
+def test_reactor():
+    reactor = Reactor()
+
+    step = 0
+    while 0 < reactor.status_percentage() < 1:
+        print(f"step {step:04d}")
+        print('\n'.join(reactor.get_statuses()))
+        reactor.auto_changes()
+        step += 1
+
+    print("Final")
+    print('\n'.join(reactor.get_statuses()))
